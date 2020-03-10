e724399b7cc5f79991beeac8fcfa6ad203057b44
Eric J Fisher <eric-fisher@pluralsight.com>
2020-03-10 15:12:23 -0400

    Updated UI, still in work

--- XAMLInXamarinForms/XAMLInXamarinForms/App.xaml.cs
+++ XAMLInXamarinForms/XAMLInXamarinForms/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XAMLInXamarinForms.Views;
 
 namespace XAMLInXamarinForms
 {
@@ -10,7 +11,7 @@ namespace XAMLInXamarinForms
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new NavigationPageView());
         }
 
         protected override void OnStart()
---
+++ XAMLInXamarinForms/XAMLInXamarinForms/Views/NavigationPageView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XAMLInXamarinForms.ViewModels;
+using XAMLInXamarinForms.Views.Routine;
+
+namespace XAMLInXamarinForms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPageView : ContentPage
+    {
+        public PlannerViewModel model = new PlannerViewModel();
+
+        public NavigationPageView()
+        {
+            InitializeComponent();
+        }
+
+        private async void FitnessButton_Clicked(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new Fitness());
+        }
+
+        private async void NutritionButton_Clicked(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new Nutrition());
+        }
+    }
+}
